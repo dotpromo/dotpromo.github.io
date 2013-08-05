4fdec76cdeea6ed49aae4afe5f0e1c2a318e2cbb
Alexander Simonov <alex@simonov.me>
2013-08-05 22:05:12 +0300

    Cleanup

--- promo-github/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PromoGithub::Application.config.secret_token = 'a18b33dab6a915153ea9b23b2d93d884694b21132b2fc8686a6c5a64fbad6e761e2cf2ed6dcd49c7fc19d588dc70186c5b7ff0093e46981030e4c5804e5811e5'
