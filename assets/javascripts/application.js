e61512a3f4c15d65ca7afc6c19b1d22374177e8d
Alexander Simonov <alex@simonov.me>
2013-08-05 22:25:02 +0300

    branch for site development

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,31 +1,33 @@
+$(document).ready(function () {
+  (function () {
+    // Hide the elements initially
+    var lis = $('.item').hide();
 
-$( document ).ready(function() {
-    (function() {
-        // Hide the elements initially
-        var lis = $('.item').hide();
+    // When some anchor tag is clicked. (Being super generic here)
+    $('.item').ready(function () {
+      var i = 0;
 
-        // When some anchor tag is clicked. (Being super generic here)
-        $('.item').ready(function() {
-            var i = 0;
-
-            // FadeIn each list item over 200 ms, and,
-            // when finished, recursively call displayImages.
-            // When eq(i) refers to an element that does not exist,
-            // jQuery will return an empty object, and not continue
-            // to fadeIn.
-            (function displayImages() {
-                lis.eq(i++).fadeIn(200, displayImages).addClass('animated fadeInLeft');
-            })();
-        });
-    })();
-    $('.backtotop').click(function(e){ $('html, body').animate({scrollTop:0}, 'slow'); e.preventDefault(); });
-    $(window).scroll(function() {
-        if ($(this).scrollTop()) {
-            $('.backtotop').fadeIn();
-        } else {
-            $('.backtotop').fadeOut();
-        }
+      // FadeIn each list item over 200 ms, and,
+      // when finished, recursively call displayImages.
+      // When eq(i) refers to an element that does not exist,
+      // jQuery will return an empty object, and not continue
+      // to fadeIn.
+      (function displayImages() {
+        lis.eq(i++).fadeIn(200, displayImages).addClass('animated fadeInLeft');
+      })();
     });
+  })();
+  $('.backtotop').click(function (e) {
+    $('html, body').animate({scrollTop: 0}, 'slow');
+    e.preventDefault();
+  });
+  $(window).scroll(function () {
+    if ($(this).scrollTop()) {
+      $('.backtotop').fadeIn();
+    } else {
+      $('.backtotop').fadeOut();
+    }
+  });
 });
 
 
